7270ef55ede4b4e302c429898c3369e93299ed47
jhpung <62597091+jhpung@users.noreply.github.com>
2023-01-03 01:16:57 +0900

    [Update] 레이아웃 수정

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link
-      className="mr-3 text-sm font-medium uppercase text-primary-600 hover:text-primary-700 dark:text-primary-500 dark:hover:text-primary-400"
+      className="text-md mr-3 font-medium uppercase text-primary-600 hover:text-primary-700 dark:text-primary-500 dark:hover:text-primary-400"
       href={`/tags/${kebabCase(text)}`}
     >
       {text.split(' ').join('-')}
--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -51,7 +51,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
             <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
+              <dt className="sr-only">작성자</dt>
               <dd>
                 <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
                   {authorDetails.map((author) => (
@@ -93,7 +93,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
               <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
                 {tags && (
                   <div className="py-4 xl:py-8">
-                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                    <h2 className="text-sm uppercase tracking-wide text-gray-500 dark:text-gray-400">
                       태그
                     </h2>
                     <div className="flex flex-wrap">
